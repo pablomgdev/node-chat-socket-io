db7f06f5adc61b070f692518f31906ad28b72d3c
pablomgdev <111525176+pablomgdev@users.noreply.github.com>
2023-08-23 14:15:34 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,6 @@
+// TODO: brew update && brew upgrade.
+// TODO: check node version, use the latest. Same with NPM.
+// TODO: init git repository.
+// TODO: init NPM project.
+// TODO: start to research and create the chat app (client and server). Make it simple.
+console.log("Hello world");
