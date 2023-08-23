0ecf8f304d66fd53ee61471cdb3a55aa9bab9c57
pablomgdev <111525176+pablomgdev@users.noreply.github.com>
2023-08-23 14:29:10 +0200

    Add eslint linter

--- index.js
+++
@@ -1,6 +0,0 @@
-// TODO: brew update && brew upgrade.
-// TODO: check node version, use the latest. Same with NPM.
-// TODO: init git repository.
-// TODO: init NPM project.
-// TODO: start to research and create the chat app (client and server). Make it simple.
-console.log("Hello world");
