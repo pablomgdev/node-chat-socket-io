a6ac13948f426b79115a7a51de181d982ab81599
pablomgdev <111525176+pablomgdev@users.noreply.github.com>
2023-08-26 22:24:09 +0200

    fix(packages/logger): Remove . from log functions because of duplications

--- packages/logger/index.mjs
+++ packages/logger/index.mjs
@@ -7,15 +7,15 @@ const LOG_TYPES = {
 };
 
 function logInfo(message) {
-  console.log(`${LOG_TYPES.INFO}: ${message}.`);
+  console.log(`${LOG_TYPES.INFO}: ${message}`);
 }
 
 function logSuccess(message) {
-  console.log(pc.green(`${LOG_TYPES.SUCCESS}: ${message}.`));
+  console.log(pc.green(`${LOG_TYPES.SUCCESS}: ${message}`));
 }
 
 function logError(message) {
-  console.log(pc.red(`${LOG_TYPES.ERROR}: ${message}.`));
+  console.log(pc.red(`${LOG_TYPES.ERROR}: ${message}`));
 }
 
 export { logInfo, logSuccess, logError };
